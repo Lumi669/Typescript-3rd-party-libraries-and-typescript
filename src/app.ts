ad46d4842ba2e9e7a3d0747b2040239d9b27cb4a
cmjiwa <wangjinghuan@yahoo.com>
2022-08-09 18:01:41 +0300

    use class transformer

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import { Product } from "./product.model";
 import "reflect-metadata";
+import { plainToClass } from "class-transformer";
 
 const products = [
   { title: "A carpet", price: 29.99 },
   { title: "A book", price: 10.99 },
+  { title: "A cake", price: 10.86 },
 ];
 
-const loadedProducts = products.map((prod) => {
-  return new Product(prod.title, prod.price);
-});
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
 
 for (const prod of loadedProducts) {
   console.log(prod.getInformation());
