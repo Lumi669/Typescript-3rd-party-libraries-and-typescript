c11b0ee5bad114e93c246070f0b4a9c9012e2e27
cmjiwa <wangjinghuan@yahoo.com>
2022-08-09 19:01:17 +0300

    Add class validator, but now not working because sttill need to do something

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,18 @@
 import { Product } from "./product.model";
 import "reflect-metadata";
-import { plainToClass } from "class-transformer";
+// import { plainToClass } from "class-transformer";
 
-const products = [
-  { title: "A carpet", price: 29.99 },
-  { title: "A book", price: 10.99 },
-  { title: "A cake", price: 10.86 },
-];
+const newPro = new Product("rose-cake", -12.99);
+console.log("newPro.getInformation = ", newPro.getInformation());
 
-// const loadedProducts = products.map((prod) => {
-//   return new Product(prod.title, prod.price);
-// });
+// const products = [
+//   { title: "A carpet", price: 29.99 },
+//   { title: "A book", price: 10.99 },
+//   { title: "A cake", price: 10.86 },
+// ];
 
-const loadedProducts = plainToClass(Product, products);
+// const loadedProducts = plainToClass(Product, products);
 
-for (const prod of loadedProducts) {
-  console.log(prod.getInformation());
-}
-
-// const p1 = new Product("A book", 12.99);
+// for (const prod of loadedProducts) {
+//   console.log(prod.getInformation());
+// }
